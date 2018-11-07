23e798fb1c937d1103beced846a27fb8ee082435
Boris Ryabov <rv.boris@gmail.com>
2018-11-07 22:40:29 +0300

    migrate to circleci

--- test/android-phone.js
+++ test/android-phone.js
@@ -83,7 +83,6 @@ test('Android Samsung', (t) => {
 
 // Source:
 // Chrome UA Spoofer
-
 test('Android Xoom', (t) => {
   const source = 'Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13'
         + ' (KHTML, like Gecko) Version/4.0 Safari/534.13';
